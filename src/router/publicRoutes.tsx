96e258a0a3d481dc38396a9701f1242e4c57187d
QUANG-SanBay <quangsanbay@gmail.com>
2026-07-25 13:51:38 +0700

    feat: FOC-7 add ForgotPassword page and integrate routing

--- src/router/publicRoutes.tsx
+++ src/router/publicRoutes.tsx
@@ -4,25 +4,30 @@ import Login from "@/pages/auth/login/Login.tsx";
 import GuestRoutes from "@/components/GuestRoutes.tsx";
 import VerifyOtp from "@/pages/auth/verify-otp/VerifyOtp.tsx";
 import CreatePassword from "@/pages/auth/create-password/CreatePassword.tsx";
+import ForgotPassword from "@/pages/auth/forgot-password/ForgotPassword.tsx";
 
 export const publicRoutes: RouteObject = {
     element: <GuestRoutes />,
     children: [
         {
             path: "login",
-            element: <Login />
+            element: <Login />,
         },
         {
             path: "register",
-            element: <Register />
+            element: <Register />,
         },
         {
             path: "verify-otp",
-            element: <VerifyOtp />
+            element: <VerifyOtp />,
         },
         {
             path: "create-password",
-            element: <CreatePassword />
+            element: <CreatePassword />,
+        },
+        {
+            path: "forgot-password",
+            element: <ForgotPassword />,
         },
     ],
 };
