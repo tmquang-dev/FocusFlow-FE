1f2fe49bbe5b6f2adad325b7b9d30e33f1c5ad60
QUANG-SanBay <quangsanbay@gmail.com>
2026-07-27 15:26:05 +0700

    feat: FOC-32 build Header component and implement logout functionality

---
+++ src/components/common/Menu.tsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { Link, useNavigate } from "react-router";
+import Button from "./Button";
+import { LogOutIcon, UserIcon } from "./Icons";
+import { authServices } from "@/api/services/authServices";
+import type { IApiLoginError } from "@/api/services/authServices.type";
+import { useAppDispatch } from "@/app/hooks";
+import { clearUser } from "@/components/profile/profileSlice";
+
+function Menu({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+
+    const handleLogOut = async () => {
+        try {
+            await authServices.logout();
+            dispatch(clearUser());
+            onClose();
+            void navigate("/login");
+        } catch (error) {
+            if (axios.isAxiosError<IApiLoginError>(error)) {
+                const responseData = error.response?.data;
+                const message = responseData?.message ?? "An unknown error occurred";
+                console.error("Logout error:", message);
+            } else {
+                console.error("An unexpected error occurred during logout");
+            }
+            dispatch(clearUser());
+            onClose();
+            void navigate("/login");
+        }
+    };
+
+    if (!isOpen) return null;
+
+    return (
+        <div className="absolute -left-29 -bottom-17 w-40 flex flex-col justify-center items-start bg-background-secondary-0 rounded-sm border border-border shadow-custom-gray overflow-hidden z-50">
+            <Link to="/profile" className="w-full">
+                <Button onClick={onClose} leftIcon={<UserIcon className="text-text-main" />} variant="outlined" className="text-text-main py-2 px-4 rounded-none w-full justify-start font-text-default">
+                    Profile
+                </Button>
+            </Link>
+            <Button onClick={() => { void handleLogOut(); }} leftIcon={<LogOutIcon className="text-text-main" />} variant="outlined" className="text-text-main py-2 px-4 rounded-none w-full justify-start font-text-default">
+                Log out
+            </Button>
+        </div>
+    );
+}
+
+export default Menu;
--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router"
+import Header from "./header/Header"
+import Footer from "./footer/Footer"
 function MainLayout() {
     return (
         <div>
-            <header>This is header</header>
+            <Header />
             <Outlet />
-            <footer>This is footer</footer>
+            <Footer />
         </div>
     )
 }
